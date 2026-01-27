734162b949dd18bae6b64ddb6ea526b6beb4da32
Michelle Si <ms1018@duke.edu>
2026-01-27 18:26:44 -0500

    updates

---
+++ pairwise_data.py
@@ -0,0 +1,306 @@
+# Auto-generated from tryout.csv
+# Feature order for x_left / x_right:
+#   [elderlyDep, lifeYearsGained, obesity, weeklyWorkhours, yearsWaiting]
+
+data = [
+    {
+        'x_left': [0, 10, 4, 0, 5],
+        'x_right': [1, 25, 0, 0, 3],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [0, 20, 1, 50, 1],
+        'x_right': [3, 20, 1, 30, 5],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [0, 20, 3, 20, 5],
+        'x_right': [0, 20, 0, 30, 3],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [0, 25, 3, 20, 1],
+        'x_right': [2, 5, 4, 0, 5],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [0, 25, 3, 20, 1],
+        'x_right': [3, 5, 3, 20, 5],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [0, 25, 4, 10, 1],
+        'x_right': [1, 20, 3, 10, 5],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [0, 5, 0, 20, 5],
+        'x_right': [3, 10, 3, 20, 5],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 10, 1, 20, 1],
+        'x_right': [2, 5, 0, 40, 5],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 10, 1, 10, 5],
+        'x_right': [0, 20, 2, 10, 3],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 10, 2, 0, 7],
+        'x_right': [0, 5, 4, 30, 5],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 20, 2, 40, 3],
+        'x_right': [3, 5, 3, 10, 5],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 20, 2, 0, 5],
+        'x_right': [0, 5, 3, 10, 3],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 20, 2, 30, 5],
+        'x_right': [3, 10, 4, 50, 7],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 20, 2, 50, 5],
+        'x_right': [3, 5, 2, 30, 5],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 20, 2, 0, 7],
+        'x_right': [1, 20, 2, 50, 5],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 25, 1, 10, 3],
+        'x_right': [2, 10, 3, 30, 7],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 25, 4, 30, 3],
+        'x_right': [3, 10, 1, 0, 3],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 25, 0, 0, 5],
+        'x_right': [1, 20, 1, 50, 3],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 25, 0, 10, 7],
+        'x_right': [0, 25, 3, 30, 7],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 5, 0, 0, 3],
+        'x_right': [2, 25, 4, 0, 5],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 5, 0, 30, 3],
+        'x_right': [3, 25, 1, 10, 7],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 5, 0, 10, 5],
+        'x_right': [1, 5, 4, 20, 3],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 5, 2, 50, 5],
+        'x_right': [1, 5, 2, 20, 7],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 5, 4, 20, 5],
+        'x_right': [1, 20, 0, 0, 1],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [2, 10, 3, 10, 1],
+        'x_right': [0, 20, 0, 40, 7],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [2, 10, 3, 40, 7],
+        'x_right': [0, 20, 4, 0, 1],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [2, 20, 1, 50, 3],
+        'x_right': [1, 20, 3, 20, 5],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [2, 20, 3, 20, 5],
+        'x_right': [2, 10, 0, 0, 1],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [2, 25, 3, 10, 5],
+        'x_right': [2, 20, 4, 20, 3],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [2, 5, 2, 20, 1],
+        'x_right': [0, 20, 0, 10, 3],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [2, 5, 4, 0, 1],
+        'x_right': [2, 10, 4, 30, 5],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [2, 5, 4, 40, 5],
+        'x_right': [0, 10, 2, 10, 3],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [2, 5, 4, 40, 7],
+        'x_right': [0, 20, 3, 50, 1],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [3, 10, 2, 0, 3],
+        'x_right': [1, 10, 3, 40, 5],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [3, 10, 1, 40, 7],
+        'x_right': [2, 25, 0, 20, 5],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [3, 20, 3, 40, 3],
+        'x_right': [2, 20, 3, 20, 3],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [3, 20, 2, 20, 7],
+        'x_right': [0, 25, 4, 40, 7],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [3, 25, 0, 30, 1],
+        'x_right': [2, 25, 1, 10, 1],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [3, 25, 2, 0, 1],
+        'x_right': [3, 20, 0, 10, 3],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [3, 25, 3, 50, 1],
+        'x_right': [3, 20, 2, 40, 3],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [3, 25, 0, 20, 7],
+        'x_right': [1, 20, 2, 40, 3],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [3, 5, 4, 30, 1],
+        'x_right': [1, 5, 1, 0, 5],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [3, 5, 0, 0, 3],
+        'x_right': [2, 25, 3, 30, 5],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [3, 5, 3, 50, 3],
+        'x_right': [0, 20, 1, 40, 7],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [3, 5, 3, 0, 5],
+        'x_right': [1, 20, 1, 0, 5],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [3, 5, 4, 40, 7],
+        'x_right': [3, 10, 1, 30, 7],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [2, -1, 0, 40, 5],
+        'x_right': [0, 20, 1, 30, 3],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, -1, 1, 40, 7],
+        'x_right': [0, 15, 2, 30, 7],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [0, 5, 2, 40, 3],
+        'x_right': [2, 20, 1, 30, 5],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [0, 5, 2, 40, 3],
+        'x_right': [2, 20, 1, 30, 5],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 20, 2, 30, 5],
+        'x_right': [0, 10, 4, 0, 5],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [1, 20, 2, 30, 5],
+        'x_right': [0, 10, 4, 0, 5],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [0, 10, 1, 30, 7],
+        'x_right': [0, 10, 4, 20, 1],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [0, 10, 1, 30, 7],
+        'x_right': [0, 10, 4, 20, 1],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [2, 25, 1, 10, 5],
+        'x_right': [1, 20, 2, 30, 7],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [2, 25, 1, 10, 5],
+        'x_right': [1, 20, 2, 30, 7],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [0, 20, 3, 40, 3],
+        'x_right': [1, 10, 0, 20, 5],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [0, 20, 3, 40, 3],
+        'x_right': [1, 10, 0, 20, 5],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [0, 5, 1, 20, 7],
+        'x_right': [2, 10, 3, 30, 3],
+        'choice': 0,  # 0 = left, 1 = right
+    },
+    {
+        'x_left': [0, 5, 1, 20, 7],
+        'x_right': [2, 10, 3, 30, 3],
+        'choice': 1,  # 0 = left, 1 = right
+    },
+]
